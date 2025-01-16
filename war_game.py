4f1abc283c63536f9d7883ec4726016e6a7e8a31
E Reynaga <57419748+Prism001@users.noreply.github.com>
2025-01-16 13:39:52 -0600

    Change Five - war_game.py

--- war_game.py
+++ war_game.py
@@ -205,7 +205,7 @@ class War_Player:
     def __init__(self, name):
         self.hand = []
         self.discard = []
-        self.record = [0,0] # Wins/Losses
+        self.record = [1,0] # Wins/Losses   # change five
         self.myname = name
 
     def get_name(self):
